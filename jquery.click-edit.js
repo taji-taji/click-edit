9b01de316a5a34eac53d7a3328677e79796a7e1a
Yutaka <y.tajika529@gmail.com>
2014-05-25 22:37:05 +0900

    modified
    double click

--- jquery.click-edit.js
+++ jquery.click-edit.js
@@ -2,14 +2,51 @@
 
 	$.fn.ce = function(options) {
 
-        var defaults={
-            defColor:"#ff6699",
-            defPadding:5
-        }
+		var defaults = {
+			defColor: "#ff6699",
+			defPadding: 5
+		}
 
-        var setting = $.extend(defaults, options);
-        
-        return this.append('<input type="text" value="">');
+		var setting = $.extend(defaults, options);
+
+		var count = 0;
+
+		return this.each(function() {
+			
+			var edit = $(this);
+			edit.data('ce', count);
+
+			var editOffset = edit.offset();
+			var editTop = editOffset.top;
+			var editLeft = editOffset.left;
+
+			$('body').append('<input id="ce-'+count+'">');
+			var editBox = $('#ce-'+count);
+			editBox.hide();
+			editBox.addClass('ce');
+			editBox.val(edit.text());
+			editBox.css({
+				'position': 'absolute',
+				'top': editTop + 'px',
+				'left': editLeft + 'px'
+			})
+
+			edit.on('dblclick', function() {
+				$(this).hide();
+				editBox.show();
+			});
+
+			$(document).on('click', function(event) {
+				if (!$.contains($('#ce-'+count)[0], event.target)) {
+					console.log($(this));
+					editBox.hide();
+					edit.show();
+				}
+			});
+
+			count++;
+
+		});
 
 	}
 
